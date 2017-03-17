1891b2ce5927d0116a8d2a516bc7546c364b669a
JJ <jbernhardt@fwparker.org>
2017-03-17 14:44:41 -0500

    two done

--- chi_life_problem.py
+++ chi_life_problem.py
@@ -1,8 +1,21 @@
 import csv
 
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Patch
+
 with open("chi_life_expectancy.txt") as tsv:
-    for line in csv.reader(tsv, dialect="excel-tab"):
-        print(line)
+    data = list(csv.reader(tsv, dialect="excel-tab"))
+
+plt.figure(figsize=[5, 10], tight_layout=True)
+
+labels = list()
+life_expectancy_2010_list = list()
+for dat in data[1:]:
+    labels.append(dat[1])
+    life_expectancy_2010_list.append(float(dat[8]))
+
+print(labels)
 
 # Make appropriate lists for plotting. Community name will be the x and 2010 life expectancy on the y.
 # 4 Plot the life_expectancy_2010_list vs a numpy arange() as a bar graph
@@ -18,3 +31,27 @@ with open("chi_life_expectancy.txt") as tsv:
 
 # Note:  If you would like to present something different than the above for your graph using this dataset,
 # just let me know your intentions before you start and I will do my best to support you.
+colors = ['red', 'blue', 'green']
+color_list = [colors[0] for i in life_expectancy_2010_list]
+color_list[life_expectancy_2010_list.index(max(life_expectancy_2010_list))] = colors[1]
+color_list[life_expectancy_2010_list.index(min(life_expectancy_2010_list))] = colors[2]
+
+# Data to make your bar graph
+# y and x must be same length and order (as always)
+y_data = life_expectancy_2010_list
+x_labels = labels
+
+# Make a bar graph
+plt.barh(np.arange(len(y_data)), y_data, 0.8, color=color_list)
+
+plt.title("Li$\int$e $\mathfrak{Expectancy}$ of Chicago 2010")
+# # Labels your bars on the x axis
+plt.yticks(np.arange(len(y_data)), x_labels, rotation=0)  # x,y,kwarg
+#
+plt.xlabel("Years")
+plt.ylabel("Neighborhood")
+
+plt.legend(
+    handles=[Patch(color=a, label=f) for a, f in [["blue", "max"], ["green", "min"]]])
+
+plt.show()
